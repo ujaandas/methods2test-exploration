d12c47859de903c371762ff88b551fd5426befc3
Ujaan Das <ujaandas03@gmail.com>
2025-03-25 15:29:24 +0800

    init flake and dev env

---
+++ START.md
@@ -0,0 +1,2 @@
+1. `git lfs pull`
+2. `tar -xvjf dataset/FILENAME.tar.bz2`
---
+++ flake.nix
@@ -0,0 +1,38 @@
+{
+  description = "Example development environment flake";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+        packages = with pkgs; [
+          git-lfs
+          python311
+        ];
+      in
+      {
+        devShell = pkgs.mkShell {
+          buildInputs = packages;
+          shellHook = ''
+            echo "Welcome to the development shell!"
+            if ! pip --version &>/dev/null; then
+              python -m venv .venv
+              .venv/bin/pip install -r requirements.txt
+            fi
+            source .venv/bin/activate
+          '';
+        };
+      }
+    );
+}
